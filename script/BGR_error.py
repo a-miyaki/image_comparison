69a6f2c682248555603e5456d0cb650aad487efb
Akira Miyaki <42761934+a-miyaki@users.noreply.github.com>
2018-11-15 12:04:01 +0900

    Update and rename BGR_average.py to BGR_error.py
    np.average to np.sum

--- script/BGR_error.py
+++ script/BGR_error.py
@@ -12,17 +12,18 @@ cv.waitKey(0)
 cv.destroyAllWindows()
 
 # RGB値の平均値を求める
-average_color_per_row = np.average(img, axis=0)
+average_color_per_row = np.sum(img, axis=0)
 
-average_color = np.average(average_color_per_row, axis=0)
+average_color = np.sum(average_color_per_row, axis=0)
 
 average_color = np.uint8(average_color)
-print("BGRの平均値")
+print("BGRの合計")
 print(average_color)
 
 # numpyの配列をリスト化する
 list = average_color.tolist()
 
-error_of_image = (list[0] + list[1] + list[2]) / 256 * 100
+error_of_image = (list[0] + list[1] + list[2]) / 256 / 3 * 100
+# ( B + G + R ) / ( 256 * 3 ) * 100 ( % )
 print("エラー率(%)")
 print(error_of_image)
