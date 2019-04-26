04a5f5d3c295f4d62f9314147fff4c04ba1614a5
Akira Miyaki <42761934+a-miyaki@users.noreply.github.com>
2019-04-26 17:06:31 +0900

    Update rename.py
    手直し
    補足説明の追加

--- script/rename.py
+++ script/rename.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import os
 import cv2
-import time
 
-# パス1の指定　元画像のパスを入れる
-path1 = "E:\\pytorch\\train\MCF_7"
-# パス2の指定　保存先のパスを入れる
-path2 = "E:\\pytorch\\train\MCF-7"
+# パス1の指定　ファイルの入ったフォルダを指定
+path1 = "E:\\pytorch\\train\\MCF_7"
+# パス2の指定　保存先のフォルダを指定
+path2 = "E:\\pytorch\\train\\MCF-7"
 
 
 def main():
@@ -16,15 +13,17 @@ def main():
         os.mkdir(path2)
 
     i = 1
-    for x in os.listdir(path1):
+    for x in os.listdir(path1):# 指定したフォルダからファイルを取得(x)
+        # パスとファイル名を結合してフルパスにする
         img_name1 = path1 + "\\" + x
+        # ファイルの確認
         print(x)
+        # ファイルを読み込む
         img = cv2.imread(img_name1, 1)
         # 名前の変更
-        cv2.imwrite(path2 + "/MCF" + '{0:04d}'.format(i) + ".png", img)
-        img_name2 = path2 + "/MCF" + '{0:04d}'.format(i) + ".png"
+        cv2.imwrite(path2 + "\\MCF" + '{0:04d}'.format(i) + ".png", img)
+        img_name2 = path2 + "\\MCF" + '{0:04d}'.format(i) + ".png"
         print(img_name2)
-        time.sleep(1)
         i += 1
 
 
