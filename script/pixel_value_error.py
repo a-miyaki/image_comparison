50327bdf5e375af2dadb913cfc0a8c206ec3ed53
Akira Miyaki <42761934+a-miyaki@users.noreply.github.com>
2019-11-13 15:07:29 +0900

    Update pixel_value_error.py

--- script/pixel_value_error.py
+++ script/pixel_value_error.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 import os
 import argparse
 import csv
@@ -11,33 +10,7 @@ parser.add_argument('--iter', type=int, default=100, help='iteration of out imag
 args = parser.parse_args()
 
 
-def make_error_map(image_A, image_B, no):
-    """
-    This function make absolut error map, image to image.
-    If you don't want to save error map, you need to add # front cv.imwrite().
-    """
-    # If image size is difficult, cv2.absdiff() will report error.
-    """print(image_A.shape)
-    print(image_B.shape)"""
-
-    # make error map
-    error_map = cv.absdiff(image_A, image_B, dst=None)
-
-    # show image
-    cv.imshow('error_map', error_map)
-
-    # save image
-    cv.imwrite('{}/error_map_{}.png'.format(args.path, no), error_map)
-
-    # wait for input key, and close all windows
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-    return error_map
-
-
-def split_RGB(no):
-    img = Image.open('{}/error_map_{}.png'.format(args.path, no))
+def split_RGB(img):
     img = np.array(img)
     
     img_R = img.copy()
@@ -46,33 +19,40 @@ def split_RGB(no):
     img_G[:, :, (0, 2)] = 0
     img_B = img.copy()
     img_B[:, :, (0, 1)] = 0
-    retun img_R, img_G, img_B
-
-
-def record(filename, iter, R, G, B, Rstd, Gstd, Bstd):
-    f = open(filename, 'a')
-    writer = csv.writer(f)
-    writer.writerow([iter, R, G, B, Rstd, Gstd, Bstd])
-    f.close()
+    return img_R, img_G, img_B
 
 
 def main():
+    filename = args.path + '/pixel_value_error.csv'
+    
     print(' Strat calcuration of pixel value error')
+    with open(filename, 'a') as f:
+        fieldname = ['iter', 'R', 'G', 'B', 'Rstd', 'Gstd', 'Bstd']
+        writer = csv.DictWriter(f, fieldnames=fieldname)
+        writer.writeheader()
+        f.close()
+    
     for i in range(1, len(os.listdir(args.path)) / 3):
-        img_a = cv2.imread('{}/image_gen_{0:08d}.png'.format(args.path, i * args.iter), 1)
-        img_b = cv2.imread('{}/image_gt_{0:08d}.png'.format(args.path, i * args.iter), 1)
+        img_a = Image.open('{}/image_gen_{0:08d}.png'.format(args.path, i * args.iter))
+        img_b = Image.open('{}/image_gt_{0:08d}.png'.format(args.path, i * args.iter))
         
-        error_map = make_error_map(img_a, img_b, i * args.iter)
-        R, G, B = split_RGB(i * args.iter)
-        R_error = np.sum(R / 256) * 100
-        G_error = np.sum(G / 256) * 100
-        B_error = np.sum(B / 256) * 100
-        R_std = np.std(R)
-        G_std = np.std(G)
-        B_std = np.std(B)
-        record('{}/result.csv'.format(args.path), i * args.iter, R_error, G_error, B_error, R_std, G_std, B_std)
-    print('Finish calcuration')
-
+        img_a_R, img_a_G, img_a_B = split_RGB(img_a)
+        img_b_R, img_b_G, img_b_B = split_RGB(img_b)
+        
+        R_error = np.sum(abs(((img_a_R + 1) - (img_b_R + 1)) / (img_b_R + 1))) / 256 / 256
+        G_error = np.sum(abs(((img_a_G + 1) - (img_b_G + 1)) / (img_b_G + 1))) / 256 / 256
+        B_error = np.sum(abs(((img_a_B + 1) - (img_b_B + 1)) / (img_b_B + 1))) / 256 / 256
+        R_std = np.std(abs(img_a_R - img_b_R))
+        G_std = np.std(abs(img_a_G - img_b_G))
+        B_std = np.std(abs(img_a_B - img_b_B))
+        
+        with open(filename, 'a') as f:
+            fieldname = ['iter', 'R', 'G', 'B', 'Rstd', 'Gstd', 'Bstd']
+            writer = csv.DictWriter(f, fieldnames=fieldname)
+            writer.writeheader()
+            writer.writerow(['iter': iter, 'R': R, 'G': G, 'B': B, 'Rstd': Rstd, 'Gstd': Gstd, 'Bstd': Bstd])
+            f.close()
+        
 
 if __name__ == '__main__':
     main()
